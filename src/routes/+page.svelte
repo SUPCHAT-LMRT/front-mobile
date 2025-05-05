5d2d8780eb6a0911b34b90aee44e6a2bb04ee177
Mattéo <bl4ise.chr@gmail.com>
2025-05-05 15:28:49 +0200

    First commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+
+	let counter = $state(0);
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<div class="mx-auto h-screen w-full bg-red-400">
+	<span class="text-white">coucu</span>
+	<Button onclick={() => (counter += 1)}>Shadcn Click me {counter}</Button>
+</div>
---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA({
+			registerType: 'autoUpdate',
+			manifest: {
+				name: 'Slack PWA',
+				short_name: 'Slack',
+				start_url: '/',
+				display: 'standalone',
+				background_color: '#ffffff',
+				theme_color: '#0f172a',
+				icons: [
+					{
+						src: '/icon-192x192.png',
+						sizes: '192x192',
+						type: 'image/png'
+					},
+					{
+						src: '/icon-512x512.png',
+						sizes: '512x512',
+						type: 'image/png'
+					}
+				]
+			}
+		})
+	]
+});
