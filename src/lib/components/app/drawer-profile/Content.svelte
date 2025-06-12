d30689d1a40dee4238e1a4530e4da91959d84fe1
lucas <lucas.moreau@supinfo.com>
2025-06-12 17:49:43 +0200

    feat: implement admin management drawer with user, postes, and invitations sections

--- src/lib/components/app/drawer-profile/Content.svelte
+++ src/lib/components/app/drawer-profile/Content.svelte
@@ -8,6 +8,11 @@
 	import { cn } from '$lib/utils';
 	import { BellOff, LaptopMinimal, Mail, Smile, User, UserCheck, UserX } from 'lucide-svelte';
 	import { Separator } from "$lib/components/ui/separator/index.js";
+	import * as Drawer from '$lib/components/ui/drawer/index.js';
+	import ContentUserAdmin from '$lib/components/app/drawer-admin/user/ContentUserAdmin.svelte';
+	import ContentPostesAdmin from '$lib/components/app/drawer-admin/postes/ContentPostesAdmin.svelte';
+	import ContentInvitationsAdmin from '$lib/components/app/drawer-admin/invitations/ContentInvitationsAdmin.svelte';
+
 
 
 	const {authenticatedUserState} = page.data as {
@@ -89,19 +94,58 @@
 	<Separator class="my-4" />
 	<p class="text-gray-500 px-2">Admin</p>
 	<div class="px-4 flex flex-col gap-2 justify-start items-start w-full">
-		<Button variant="option">
-			<User />
-			Gestion des utilisateurs
-		</Button>
 
-		<Button variant="option">
-			<LaptopMinimal />
-			Gestion des postes
-		</Button>
+		<Drawer.NestedRoot setBackgroundColorOnScale={false}>
+			<Drawer.Trigger>
+				<Button variant="option">
+					<User />
+					Gestion des utilisateurs
+				</Button>
+			</Drawer.Trigger>
+			<Drawer.Portal>
+				<Drawer.Overlay class="fixed inset-0 bg-black/40" />
+				<Drawer.Content
+					class="fixed right-0 bottom-0 left-0 mt-24 flex h-full max-h-[96%] flex-col rounded-t-[10px]"
+				>
+					<ContentUserAdmin />
+				</Drawer.Content>
+			</Drawer.Portal>
+		</Drawer.NestedRoot>
 
-		<Button variant="option">
-			<Mail />
-			Gestion des invitations
-		</Button>
+
+		<Drawer.NestedRoot setBackgroundColorOnScale={false}>
+			<Drawer.Trigger>
+				<Button variant="option">
+					<LaptopMinimal />
+					Gestion des postes
+				</Button>
+			</Drawer.Trigger>
+			<Drawer.Portal>
+				<Drawer.Overlay class="fixed inset-0 bg-black/40" />
+				<Drawer.Content
+					class="fixed right-0 bottom-0 left-0 mt-24 flex h-full max-h-[96%] flex-col rounded-t-[10px]"
+				>
+					<ContentPostesAdmin />
+				</Drawer.Content>
+			</Drawer.Portal>
+		</Drawer.NestedRoot>
+
+
+		<Drawer.NestedRoot setBackgroundColorOnScale={false}>
+			<Drawer.Trigger>
+				<Button variant="option">
+					<Mail />
+					Gestion des invitations
+				</Button>
+			</Drawer.Trigger>
+			<Drawer.Portal>
+				<Drawer.Overlay class="fixed inset-0 bg-black/40" />
+				<Drawer.Content
+					class="fixed right-0 bottom-0 left-0 mt-24 flex h-full max-h-[96%] flex-col rounded-t-[10px]"
+				>
+					<ContentInvitationsAdmin />
+				</Drawer.Content>
+			</Drawer.Portal>
+		</Drawer.NestedRoot>
 	</div>
 </div>
